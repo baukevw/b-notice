59d8af6164c9e1ab406b8552872eedb8f6de3b3e
baukevw <bauke@baukevanwieren.nl>
2018-02-02 14:45:06 +0100

    skip_before_action

--- app/controllers/uptime_controller.rb
+++ app/controllers/uptime_controller.rb
@@ -1,5 +1,5 @@
 class UptimeController < ApplicationController
-  skip_before_filter :verify_authenticity_token, if: :json_request?
+  skip_before_action :verify_authenticity_token, if: :json_request?
 
   def uptime_robot_endpoint
     logger.info params
