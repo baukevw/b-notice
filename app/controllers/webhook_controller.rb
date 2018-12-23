46683d04d5eaa7104a00b3ed87619e3332102408
baukevw <bauke@baukevanwieren.nl>
2018-12-23 21:55:58 +0100

    csrf issue 2

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,10 +1,8 @@
 class WebhookController < ApplicationController
-  protect_from_forgery with: :null_session
-
-  respond_to :json
+  skip_before_action :verify_authenticity_token
 
   def rpi_relay_webhook
-    logger.info params.rpi_relay_api
+    logger.info request
     render status: 200, json: { 'Status' => '200' }.to_json
   end
 end
