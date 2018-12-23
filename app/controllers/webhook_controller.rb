b3adcfd99a886b8eaaf5db0e3e0431919624eef9
baukevw <bauke@baukevanwieren.nl>
2018-12-23 21:49:48 +0100

    webhook update

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -2,6 +2,7 @@ class WebhookController < ApplicationController
   protect_from_forgery with: :null_session, only: :rpi_relay_webhook
 
   def rpi_relay_webhook
-    logger.info params.inspect
+    logger.info params.rpi_relay_api
+    render status: 200, json: { 'Status' => '200' }.to_json
   end
 end
